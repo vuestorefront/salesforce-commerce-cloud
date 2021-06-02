1fcfde3f8d2b1f113d993f237d8297d39f18dfba
Pavel Pavlov <pavel.pavlov@aspectworks.com>
2021-06-02 14:09:42 +0300

    Auto publish

---
+++ scripts/publishBeta.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const { exec } = require('child_process');
+
+const packagesBaseDir = path.join(process.cwd(), 'packages');
+const publishPackages = ['api-client', 'composables'];
+
+publishPackages.forEach((repo) => {
+  const repoPath = path.join(packagesBaseDir, repo);
+
+  exec(`npm publish ${repoPath} --access public --tag beta --dry-run`, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`error: ${error.message}`);
+      return;
+    }
+    if (stderr) {
+      console.log(`stderr: ${stderr}`);
+      return;
+    }
+    console.log(`stdout: ${stdout}`);
+  });
+});
