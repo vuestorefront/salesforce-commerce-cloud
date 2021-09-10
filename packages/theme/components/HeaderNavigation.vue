94052c060d81f301e407e8317f0dc9f8dd337be1
Pavel Pavlov <pavel@forkpoint.com>
2021-09-10 21:57:03 +0300

    Update the nav menu component structure

---
+++ packages/theme/components/HeaderNavigation.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="sf-header__navigation desktop" v-if="!isMobile">
+    <SfHeaderNavigationItem
+      v-for="(category, index) in categories"
+      :key="index"
+      class="nav-item"
+      v-e2e="`app-header-url_${category.id}`"
+      :label="category.name"
+      :link="localePath(`/c/${category.id}`)"
+    />
+  </div>
+  <SfModal v-else :visible="isMobileMenuOpen">
+    <SfHeaderNavigationItem
+      v-for="(category, index) in categories"
+      :key="index"
+      class="nav-item"
+      v-e2e="`app-header-url_${category.id}`"
+    >
+      <template #mobile-navigation-item>
+        <SfMenuItem
+          :label="category"
+          class="sf-header-navigation-item__menu-item"
+          :link="localePath(`/c/${category.id}`)"
+          @click.native="toggleMobileMenu"
+        />
+      </template>
+    </SfHeaderNavigationItem>
+  </SfModal>
+</template>
+
+<script>
+import { SfMenuItem, SfModal } from '@storefront-ui/vue';
+import { useCategory } from '@vue-storefront/sfcc';
+import { computed } from '@vue/composition-api';
+import { onSSR } from '@vue-storefront/core';
+import { useUiState } from '~/composables';
+
+export default {
+  name: 'HeaderNavigation',
+  components: {
+    SfMenuItem,
+    SfModal
+  },
+  props: {
+    isMobile: {
+      type: Boolean,
+      default: false
+    }
+  },
+  setup() {
+    const { search: searchCategories, categories: menuCategories } = useCategory();
+    const { isMobileMenuOpen, toggleMobileMenu } = useUiState();
+
+    const categories = computed(() => {
+      if (menuCategories && menuCategories.value) {
+        return menuCategories.value;
+      }
+
+      return [];
+    });
+
+    onSSR(async () => {
+      await searchCategories({ slug: 'root', target: 'menu' });
+    });
+
+    return {
+      categories,
+      isMobileMenuOpen,
+      toggleMobileMenu
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.sf-header-navigation-item {
+  ::v-deep &__item--mobile {
+    display: block;
+  }
+}
+.sf-modal {
+  ::v-deep &__bar {
+    display: none;
+  }
+  ::v-deep &__content {
+    padding: var(--modal-content-padding, var(--spacer-base) 0);
+  }
+}
+</style>
